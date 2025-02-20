9935d6ed2fc00d98fa7a05e5b60c78404c638d6e
HemoDemo <54177917+HemoDemo@users.noreply.github.com>
2025-02-20 21:41:56 +0200

    Update Todo.jsx

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -151,11 +151,11 @@ function Todos() {
           </ol>
         </section>
         <section className="status">
-          ST<span style={{minWidth:'30px'}}>{status}</span>
-          TS<span style={{minWidth:'30px'}}>{tasks.length}</span>
-          TSS<span style={{minWidth:'30px'}}>{localTasks.length}</span>
-          MSG<span style={{minWidth:'30px'}}>{msg.length === 0 ? "TRY TO ADD TASKS" : msg}</span>
-          ATN:<span style={{minWidth:'30px'}}>{addTaskNumber}</span>
+          <strong>ST</strong><span style={{minWidth:'30px'}}>{status}</span>
+          <strong>TS</strong><span style={{minWidth:'30px'}}>{tasks.length}</span>
+         <strong>TSS</strong><span style={{minWidth:'30px'}}>{localTasks.length}</span>
+         <strong>MSG</strong><span style={{minWidth:'30px'}}>{msg.length === 0 ? "TRY TO ADD TASKS" : msg}</span>
+         <strong>ATN</strong><span style={{minWidth:'30px'}}>{addTaskNumber}</span>
         </section>
         <section className="lastLine">
           <button className="rest" onClick={() => setLocalTasks([])}>
