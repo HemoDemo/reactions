2144255ccedaf593d3a5258539fc359544c7d0b3
HemoDemo <54177917+HemoDemo@users.noreply.github.com>
2025-02-21 20:10:24 +0200

    Update Todo.jsx

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from "react";
-import "./todo.css";
-import Refresh from './Refresh';
+import "./todos-v-0.2.0.css";
+import Button from "./Button";
 import Himo from "./himo.png";
 function Todos() {
   const getTasks = () => {
@@ -15,13 +15,14 @@ function Todos() {
   const focus = useRef(null);
   const [tasks, setTasks] = useState(getTasks());
   const [localTasks, setLocalTasks] = useState(getTasks());
-  const [msg, setMsg] = useState(["NO TASKS ADDED"]);
+  const [msg, setMsg] = useState(["..."]);
   const [newTask, setNewTask] = useState("");
-  const [status, setStatus] = useState("START");
+  const [status, setStatus] = useState("ابدأ");
   const [addTaskNumber, setAddTaskNumber] = useState(0);
 
   useEffect(() => {
     focus.current.focus();
+    setStatus("صندوق اضافة");
   }, []);
 
   useEffect(() => {
@@ -30,31 +31,46 @@ function Todos() {
 
   function change(e) {
     setNewTask(e.target.value);
+    setStatus("الكتابة ...");
   }
+  const inputOnclick = () => {
+    setStatus("صندوق اضافة");
+  };
+  const refreshOnClick = () => {
+    localTasks.length !== 0
+      ? setStatus("استعادة المهام")
+      : setStatus("اعادة تشغيل");
+  };
+  const refreshOnunClick = () => {
+    setStatus("...");
+  };
 
   function add() {
-    setStatus("adding");
     if (newTask.trim() !== "") {
+      setStatus("اضف مهمة");
       setTasks((t) => [...t, newTask]);
       setLocalTasks((t) => [...t, newTask]);
       addTaskNumber > -1 && setAddTaskNumber(addTaskNumber + 1);
     }
-    if (newTask.trim() === "") setMsg(["Add tasks to do"]);
-    setNewTask("");
+    if (newTask.trim() === "") {
+      setMsg(["أضف مهمة"]);
+      setStatus("تنبيه");
+    }
+    setNewTask("  ");
   }
   function del(index) {
-    setStatus("deleting");
+    setStatus("حذف");
     const t = tasks.filter((task, i) => i !== index);
     setTasks(t);
     addTaskNumber > 0 && setAddTaskNumber(addTaskNumber - 1);
   }
   function delM(index) {
-    setStatus("deleting");
+    setStatus("ازالة التنبيه");
     const ms = msg.filter((m, i) => i !== index);
     setMsg(ms);
   }
   function up(index) {
-    setStatus("ascending");
+    setStatus("تصاعدي");
     if (index > 0) {
       const t = [...tasks];
       [t[index - 1], t[index]] = [t[index], t[index - 1]];
@@ -62,13 +78,19 @@ function Todos() {
     }
   }
   function down(index) {
-    setStatus("desending");
+    setStatus("تنازلي");
     if (index < tasks.length - 1) {
       const t = [...tasks];
       [t[index], t[index + 1]] = [t[index + 1], t[index]];
       setTasks(t);
     }
   }
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+  const emptyTasks = () => {
+    setTasks([]);
+  };
 
   return (
     <>
@@ -84,16 +106,24 @@ function Todos() {
           <span className="txt6"></span>
           <span className="txt5"></span>
           <span className="txt4"></span>
-          <Refresh />
+          <Button
+            hint="تحميل"
+            onMouseOver={refreshOnClick}
+            onMouseOut={refreshOnunClick}
+            onClick={handleRefresh}
+          >
+            💿
+          </Button>
         </section>
         <section className="entry">
           <input
             type="text"
-            placeholder="add task ..."
+            placeholder="اضف مهمة ..."
             value={newTask}
             onChange={change}
             className="tasks-entry tasks-entry-big"
             ref={focus}
+            onFocus={inputOnclick}
           />
           <button
             className={
@@ -107,18 +137,21 @@ function Todos() {
         <section className="tasks-shown-start">
           <h2>TASKS...</h2>
           <p>
-            {tasks.length === 0
-              ? `no tasks shown`
-              : localTasks.length === 0 ?
-              `try to add tasks`
-              : `You still have ${tasks.length} to do`}
+            {tasks.length === 0 && localTasks.length === 0
+              ? `أضف مهام`
+              : tasks.length === 0
+              ? `المهام ليست معروضه`
+              : tasks.length === 1
+              ? `مازال لديك مهمة للقيام بها`
+              : tasks.length === 2
+              ? `مازال لديك مهمتان للقيام بهما`
+              : `مازال لديك ${tasks.length} مهام للقيم بها`}
           </p>
         </section>
         <section className="tasks-shown tasks-big-shown">
           <ol className="tasks-container">
             {tasks.map((task, index) => (
               <li key={index}>
-                
                 <div className="task-controls">
                   <button className="del-btn" onClick={() => del(index)}>
                     x
@@ -131,68 +164,63 @@ function Todos() {
                   </button>
                 </div>
                 <div className="txt-content">
-                  <h2 className="task-txt">{task}</h2>
+                  <h2 lang="ar" className="task-txt">
+                    {task}
+                  </h2>
                 </div>
               </li>
             ))}
             {tasks.length === 0 &&
               msg.map((m, i) => (
                 <li key={i} className="d-father">
-                  <div className="txt-content">
-                    <h2 className="task-txt">{m}</h2>
-                  </div>
-                  <div className="task-controls tsk-cont-d">
+                  <div className="tsk-cont-d">
                     <button className="del-btn d" onClick={() => delM(i)}>
                       x
                     </button>
+                  </div>
+                  <div className="txt-content">
+                    <h2 className="task-txt">
+                      {m === "..." &&
+                      tasks.length === 0 &&
+                      localTasks.length === 0
+                        ? `لايوجد مهام`
+                        : tasks.length === 0 && localTasks.length > 0
+                        ? "المهام غير معروضه"
+                        : m}
+                    </h2>
                   </div>
                 </li>
               ))}
           </ol>
         </section>
         <section className="status">
-          <h6 className='span status-part'>
-        
-            <span className='values status-value'>
-              {status}
-            </span>
+          <h6 className="span status-part">
+            <span className="values status-value">{status}</span>
           </h6>
-          <h6 className='span tasks-part'>
-            <span className='strong tasks-title'>
-              TS
-            </span>
-              <span className='values tasks-value'>
-                {tasks.length}
-              </span>
+          <h6 className="span tasks-part">
+            <span className="tasks-title">TS</span>
+            <span className="values tasks-value">{tasks.length}</span>
           </h6>
-          <h6 className='span tasks-stored-part'>
-            <span className='strong tasks-stored-title'>
-            TSS
-            </span>
-            <span className='values tasks-stored-value'>
+          <h6 className="span tasks-stored-part">
+            <span className="tasks-stored-title">TSS</span>
+            <span className="values tasks-stored-value">
               {localTasks.length}
             </span>
           </h6>
-          <h6 className='span msg-part'>
-            <span className='strong msg-title'>
-             M
-           </span>
-           <span className='values msg-value'>
-             {msg.length === 0 ? "TRY TO ADD TASKS" : msg}
-           </span>
-         </h6>
-         <h6 className='span tasks-added-part'>
-            <span className='strong tasks-added-title'>
-              ATN
+          <h6 className="span msg-part">
+            <span className="msg-title">M</span>
+            <span className="values msg-value">
+              {msg.length === 0 ? " أضف مهام" : msg}
             </span>
-            <span className='values tasks-added-value'>
-              {addTaskNumber}
-            </span>
+          </h6>
+          <h6 className="span tasks-added-part">
+            <span className="tasks-added-title">ATN</span>
+            <span className="values tasks-added-value">{addTaskNumber}</span>
           </h6>
         </section>
         <section className="lastLine">
           <button className="rest" onClick={() => setLocalTasks([])}>
-            REST
+            حذف المخزن
           </button>
           <button
             className="rest"
@@ -202,13 +230,13 @@ function Todos() {
               setLocalTasks([]);
             }}
           >
-            REST-ALL
+            حذف الجميع
           </button>
-          <button className="rest" onClick={() => setTasks([])}>
-            NOTASKS
+          <button className="rest" onClick={emptyTasks}>
+            الانتهاء من المهام
           </button>
           <button className="rest" onClick={() => setNewTask("")}>
-            CLEAR
+            تفريغ الصندوق
           </button>
         </section>
         <section className="app-creator">
@@ -221,4 +249,5 @@ function Todos() {
 }
 
 export default Todos;
+
 
